0d12232a8cf0b30fab6453add3836c50f51372a8
Tigerstec <leonardo06.pc.81@gmail.com>
2025-11-27 17:14:25 -0300

    Add password hashing example in teste.php

---
+++ teste.php
@@ -0,0 +1,13 @@
+<?php
+
+// A senha que queremos criptografar
+$senhaPlana = 'Mrc@1977';
+
+// Gera o hash usando o algoritmo padrão e mais seguro do seu PHP
+$hash = password_hash($senhaPlana, PASSWORD_DEFAULT);
+
+// Imprime o hash na tela
+echo "Senha Plana: " . $senhaPlana . "<br>";
+echo "Hash Gerado: " . $hash;
+
+?>
