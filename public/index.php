97167931cfc81b7e4897266f572e27e15d1da1fe
Tigerstec <leonardo06.pc.81@gmail.com>
2025-11-27 16:58:18 -0300

    Update index.php

--- public/index.php
+++ public/index.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>ALR Automações Industrial</title>
+    <link rel="shortcut icon" href="../public/assets/images/logoweb.ico">
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css">
     <link rel="stylesheet" href="../public/assets/css/index.css">
