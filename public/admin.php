5b9eeeb71de70806cd089ff762a41ad4f46ed5eb
Tigerstec <leonardo06.pc.81@gmail.com>
2025-11-27 15:57:33 -0300

    Update admin.php

--- public/admin.php
+++ public/admin.php
@@ -369,8 +369,6 @@ $is_logged_in = isset($_SESSION['user_id']);
 
     <input type="hidden" id="editingId">
                             
-    <script src="https://code.jquery.com/jquery-3.7.1.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.mask/1.14.16/jquery.mask.min.js"></script>
     <script src="../public/assets/js/admin.js"></script>
     <script>
         // Menu Mobile Toggle
